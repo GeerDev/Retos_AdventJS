d0a561372e7565cba04740f02ceea8aed2c0ca3c
Ger <gerfersie@gmail.com>
2022-12-06 21:16:06 +0100

    Challenge 06

---
+++ challenge06.js
@@ -0,0 +1,21 @@
+function createCube(size) {
+    let cadena = ''
+    for(i=0; i<=size; i++) {
+      if(i !== size) {
+        cadena += " ".repeat(size - i) + `/\\`.repeat(i) + `_\\`.repeat(size) + "\n"
+      } else {
+        cadena += `/\\`.repeat(i) + `_\\`.repeat(size) + "\n"
+      }   
+    }
+    for(i=size; i>0; i--) {
+      if (i !== size) {
+        cadena += " ".repeat(size - i) + `\\/`.repeat(i) + `_/`.repeat(size) + "\n"
+      } else {
+        cadena += `\\/`.repeat(i) + `_/`.repeat(size) + "\n"
+      }   
+    }
+    return cadena.substring(cadena.indexOf("\n"), cadena.length - 1).replace("\n", "").replace(" ", "")
+  }
+  
+  const cubeOfTwo = createCube(4)
+  console.log(cubeOfTwo)
